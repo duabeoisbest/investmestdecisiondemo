7b66c2119e807ed0764de7a3b6a60ab6c021dafc
Ngoc Linh Bui <84629810+duabeoisbest@users.noreply.github.com>
2026-08-19 23:54:24 +0200

    Update baseUrl to support platform-specific hosts
    Refactor baseUrl to adapt to web and Android environments.
    Fixing bugs: fastapi aufruf

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,10 +1,17 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../models/stock_model.dart';
 
 class ApiService {
-  // Nếu test trên máy ảo Android, hãy đổi 'localhost' thành '10.0.2.2'
-  final String baseUrl = "http://localhost:8000/api/stock";
+  // nếu test trên máy ảo Android, sử dụng '10.0.2.2' thay vì 'localhost'
+  String get baseUrl {
+    final host = kIsWeb
+        ? 'localhost'
+        : (Platform.isAndroid ? '10.0.2.2' : 'localhost');
+    return 'http://$host:8000/api/stock';
+  }
 
   Future<StockData> fetchStockAnalysis(String symbol) async {
     final response = await http.get(Uri.parse('$baseUrl/$symbol'));
